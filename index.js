8dc5b81084e7618bc2d94ca1ac85e90254e460ff
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-20 09:19:31 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//is an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+//generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) +1;
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 19;  
+let num4 = 5;
+let mod = num3 % num4;
+
+//will return 20 as the highest number in the set
+let max = Math.max(4, 5,10, 20, 18);
